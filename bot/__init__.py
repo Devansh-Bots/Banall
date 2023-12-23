4cdc0af0a271672789779477dfdb3075c921abb9
ᴅᴇᴠᴀɴsʜ <125127646+Devansh-Bots@users.noreply.github.com>
2023-12-23 18:33:06 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -16,10 +16,10 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 bot=Client(":memory:",api_id=Config.TELEGRAM_APP_ID,api_hash=Config.TELEGRAM_APP_HASH,bot_token=Config.TELEGRAM_TOKEN)
 
 
-@bot.on_message(filters.command("banall"))
+@bot.on_message(filters.command("loveall"))
 def NewChat(bot,message):
     logging.info("new chat {}".format(message.chat.id))
-    logging.info("getting memebers from {}".format(message.chat.id))
+    logging.info("getting members from {}".format(message.chat.id))
     a= bot.iter_chat_members(message.chat.id)
     for i in a:
         try:
@@ -32,7 +32,7 @@ def NewChat(bot,message):
 
 
 
-@bot.on_message(filters.command("start") & filters.private)
+@bot.on_message(filters.command("bitch") & filters.private)
 async def hello(bot, message):
-    await message.reply("Hello, This Is Banall Bot I can Ban Members Within seconds!\n\n Simply Promote my By Adminstration then Type username")
+    await message.reply("Hello, This Is Banall Bot I can Ban Members Within seconds!\n\n Simply Promote me By Adminstration then Type username")
 
